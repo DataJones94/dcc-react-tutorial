35c91af501300184e796157fe60f5caf37b87d02
unknown <k.jones45@yahoo.com>
2023-04-02 18:16:48 -0400

    worked on css for app. worked on spacing of margins. created entries chart tracker. created add entry form css.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries'
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
 import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
-
+import './App.css';
 function App() {
   const [entries, setEntries] = useState([{weight: 175, date: '11-23-2021'}, {weight: 176, date: '11-24-2021'}])
   
@@ -16,13 +16,26 @@ function App() {
 
 
   return (
-    <div>
-      <DisplayEntries parentEntries= {entries}/>
-      <AddEntryForm addNewEntry={addNewEntry}/>
-      <EntriesChartTracker parentEntries={entries}/>
+    <div className= 'container-fluid'>
+      <div className ='row'>
+        <h3 style={{margin:'1em'}}>Weight 
+        <small className= 'text-muted'>Tracker</small></h3>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <DisplayEntries parentEntries= {entries}/>
+          </div>
+        </div>
+        <div className='border-box'>
+           <AddEntryForm addNewEntry={addNewEntry}/>
+        </div>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <EntriesChartTracker parentEntries={entries}/>
+          </div>
+      </div>
+     </div>
     </div>
-
-  );
+ );
 }
 
 export default App;
